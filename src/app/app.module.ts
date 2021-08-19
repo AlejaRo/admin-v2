45e17c6614529804560cde9c816982135aae3716
Alejandra Rodriguez <rod.aleja1991@gmail.com>
2021-08-19 18:19:03 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng9RutModule } from 'ng9-rut';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng9RutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
